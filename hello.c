6d22f161cfc52bd08cf7a9ec52c1ca5f614af87f
Denden353 <65858349+Denden353@users.noreply.github.com>
2020-12-02 22:32:18 +0300

    Add files via upload

---
+++ hello.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+#include <cs50.h>
+int main (){
+    string name=get_string("İsmin Nedir?\n");
+    printf("İsmin:%s\n",name);
+}
---
+++ marioad.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <cs50.h>
+
+
+int main(void)
+{
+int h=0;
+do {
+    h=get_int("Ne kadar yükseklikte?\n");
+}while (h<1 || h>8);
+for(int j=1; j<=h; j++) {
+    for( int i= h; i>j; i--){
+        printf(""); //kırmızı
+    }
+    for( int k=1; k<=j; k++){
+        printf("#"); //siyah
+    }
+    for(int z=0; z<=1; z++ ){
+        printf(" ");
+    }
+    for(int b=1; b<=j; b++){
+ printf("#");
+    }
+    printf("\n");
+}
+}
